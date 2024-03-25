c0426afac5a1bbf1797da067f53f73e860e2cafc
Yaroslav Polyakov <yaroslaff@gmail.com>
2024-03-26 01:18:55 +0700

    new minimal app.py

--- app.py
+++ app.py
@@ -1,33 +1,15 @@
+from fastapi import FastAPI
 
-from fastapi import Depends, FastAPI
-from fastapi.staticfiles import StaticFiles
-from typing import Annotated
-from starlette.middleware.sessions import SessionMiddleware
-
-from fastapi_simple_auth import auth_router, logged_in_user
+from fastapi_simple_auth import SimpleAuth, logged_in_user
 from fastapi_simple_auth.views import User
-
-
+        
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key='ChangeMe', max_age=None)
 
-app.include_router(auth_router, prefix="/auth")
+simpleauth = SimpleAuth(app)
 
-app.mount("/static", StaticFiles(packages=['fastapi_simple_auth']), name="static")
-
-# app.mount('/auth', api_auth)
-
-@app.get("/users/me/", response_model=User)
+@app.get("/protected", response_model=User)
 async def read_users_me(
+    # user: logged_in_user
     user: logged_in_user
 ):
-    print("VIEW USER:", user)
-    print(type(user))
-    print(dir(user))
     return user
-
-@app.get("/users/me/items/")
-async def read_own_items(
-    user: logged_in_user
-):
-    return [{"item_id": "Foo", "owner": user.username}]
